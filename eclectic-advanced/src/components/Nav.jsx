6d3052a9bbeb6e0cff0d1c7af6c76f73aa131da5
Connor Sullivan <crsullivan@protonmail.com>
2020-09-22 00:23:50 -0700

    link update for deployment

--- eclectic-advanced/src/components/Nav.jsx
+++ eclectic-advanced/src/components/Nav.jsx
@@ -5,10 +5,10 @@ const Nav = () => {
 
     return (
         <div className="Nav">
-            <h1><a href="http://localhost:3000">EclecticLogic</a></h1>
+            <h1><a href="https://eclectic-logic.vercel.app/">EclecticLogic</a></h1>
             <div className="navLinks">
-                <a href="http://localhost:3000/contact">Contact</a>
-                <a href="http://localhost:3000/about">About</a>
+                <a href="https://eclectic-logic.vercel.app/contact">Contact</a>
+                <a href="https://eclectic-logic.vercel.app/about">About</a>
                 <a href="https://eclectic-blog.vercel.app/">Blog</a>
             </div>
         </div>
